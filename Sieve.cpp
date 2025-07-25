aa24ac90c58f4d0d6bcfbc47baa88791e2d3801b
AryanM65 <aryanmahajan600@gmail.com>
2025-07-25 10:33:41 +0530

    sieve

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,16 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    vector<bool> sieve(1e5 + 1, false);
-    sieve[2] = false;
-    for(int i = 2; i*i <= 1e5; i++){
+vector<bool> generate_sieve(int n){
+    vector<bool> sieve(n+1, false);
+    if(n > 1)sieve[2] = false;
+    for(int i = 2; i*i <= n; i++){
         if(!sieve[i]){
-            for(int j = i*i; j <= 1e5; j += i){
+            for(int j = i*i; j<=n; j += i){
                 sieve[j] = true;
             }
         }
     }
+    return sieve;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<bool> sieve = generate_sieve(n);
     //sieve generated
     return 0;
 }
