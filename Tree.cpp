45e87e146e7f41e4bd0885415c961e917c1fee6c
AryanM65 <aryanmahajan600@gmail.com>
2025-07-25 10:30:18 +0530

    added tree

---
+++ Tree.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(){
+        val = 0;
+        left = nullptr;
+        right = nullptr;
+    }
+    TreeNode(int val){
+        this->val = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+
+int main(){
+
+    return 0;
+}
